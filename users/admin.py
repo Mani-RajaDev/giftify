21ba0607f389e6057117e95286033581ba9cf97f
devwizard-Mani <orewazorodha@gmail.com>
2023-09-06 11:40:53 +0530

    feat(models): Add Profile and Address model

--- users/admin.py
+++ users/admin.py
@@ -1,9 +1,13 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
+from django.contrib.sessions.models import Session
 
 from .forms import CustomUserChangeForm, CustomUserCreationForm
 from .models import (
     CustomUser,
+    GuestUser,
+    Address,
+    Profile,
 )
 
 
@@ -47,3 +51,22 @@ class CustomUserAdmin(UserAdmin):
     search_fields = ["email"]
     ordering = ["email"]
     filter_horizontal = []
+
+
+@admin.register(GuestUser)
+class GuestUserAdmin(admin.ModelAdmin):
+    list_display = ["session", "get_session_expiration_date"]
+
+@admin.register(Profile)
+class ProfileAdmin(admin.ModelAdmin):
+    list_display = ["display_fullname", "mobile_number",]
+
+
+@admin.register(Address)
+class AddressAdmin(admin.ModelAdmin):
+    list_display = ["__str__", "is_guest", "mobile_number", "state"]
+
+
+@admin.register(Session)
+class SessionAdmin(admin.ModelAdmin):
+    list_display = ["session_key", "expire_date"]
--- users/models.py
+++ users/models.py
@@ -1,7 +1,9 @@
 from django.db import models
 from django.contrib import admin
 from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin
+from phonenumber_field.modelfields import PhoneNumberField
 from django.conf import settings
+from django.contrib.sessions.models import Session
 
 from .managers import CustomUserManager
 
@@ -24,3 +26,138 @@ class CustomUser(AbstractBaseUser, PermissionsMixin):
 
     def __str__(self):
         return self.email
+
+
+class GuestUser(models.Model):
+    session = models.OneToOneField(Session, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"Session ID: {self.session.session_key}"
+
+    @admin.display(description="Session Expiration Date")
+    def get_session_expiration_date(self):
+        """
+        Return the session expiration date or 'Session Expired' if not available.
+        """
+        return self.session.expire_date or "Session Expired"
+
+
+class Profile(models.Model):
+    user = models.OneToOneField(
+        settings.AUTH_USER_MODEL,
+        related_name="profile",
+        on_delete=models.CASCADE,
+        null=True,
+        blank=True,
+    )
+    first_name = models.CharField(max_length=255)
+    last_name = models.CharField(
+        max_length=255, null=True, blank=True, verbose_name="Last name (optional)"
+    )
+    mobile_number = PhoneNumberField(region="IN", max_length=13, unique=True)
+    
+    def __str__(self):
+        return self.first_name.title()
+
+    def get_last_name_or_empty(self):
+        """
+        Returns the last name if available, or an empty string if it's None.
+        """
+        return self.last_name or ""
+
+    @admin.display(description="User Name")
+    def display_fullname(self):
+        return f"{self.first_name} {self.get_last_name_or_empty()}".title()
+
+
+class Address(models.Model):
+    STATE_CHOICES = (
+        ("Andaman and Nicobar islands", "Andaman and Nicobar islands"),
+        ("Andhra Pradesh", "Andhra Pradesh"),
+        ("Arunacal Pradesh", "Arunacal Pradesh"),
+        ("Assam", "Assam"),
+        ("Bihar", "Bihar"),
+        ("Chandigarh", "Chandigarh"),
+        ("Chhattisgarh", "Chhattisgarh"),
+        ("Dadra & Nagar Haveli & Daman & Diu", "Dadra & Nagar Haveli & Daman & Diu"),
+        ("Goa", "Goa"),
+        ("Gujarat", "Gujarat"),
+        ("Haryana", "Haryana"),
+        ("Delhi", "Delhi"),
+        ("Himachal Pradesh", "Himachal Pradesh"),
+        ("Jammu & Kashmir", "Jammu & Kashmir"),
+        ("Jharkand", "Jharkand"),
+        ("Karnataka", "Karnataka"),
+        ("Kerala", "Kerala"),
+        ("Lakshadweep", "Lakshadweep"),
+        ("Madhya Pradesh", "Madhya Pradesh"),
+        ("Maharashtra", "Maharashtra"),
+        ("Manipur", "Manipur"),
+        ("Meghalaya", "Meghalaya"),
+        ("Mizoram", "Mizoram"),
+        ("Nagaland", "Nagaland"),
+        ("Odisha", "Odisha"),
+        ("Puducherry", "Puducherry"),
+        ("Punjab", "Punjab"),
+        ("Rajasthan", "Rajasthan"),
+        ("Sikkim", "Sikkim"),
+        ("Tamil Nadu", "Tamil Nadu"),
+        ("Telanga", "Telanga"),
+        ("Tripura", "Tripura"),
+        ("Uttarkhand", "Uttarkhand"),
+        ("Uttar Pradesh", "Uttar Pradesh"),
+        ("West Bengal", "West Bengal"),
+    )
+
+    user = models.OneToOneField(
+        settings.AUTH_USER_MODEL,
+        related_name="address",
+        on_delete=models.SET_NULL,
+        null=True,
+        blank=True,
+    )
+    guest_user = models.OneToOneField(
+        GuestUser,
+        related_name="guest_address",
+        on_delete=models.SET_NULL,
+        null=True,
+        blank=True,
+    )
+    name = models.CharField(max_length=255, default="Mani")
+    mobile_number = PhoneNumberField(
+        region="IN", max_length=13, default="+91908750532", unique=True
+    )
+    pincode = models.CharField(max_length=6)
+    address = models.TextField()
+    city_district_town = models.CharField(
+        max_length=255, verbose_name="City/District/Town"
+    )
+    state = models.CharField(choices=STATE_CHOICES, max_length=40)
+
+    # Optional fields
+    landmark = models.CharField(
+        max_length=255, null=True, blank=True, verbose_name="Landmark (optional)"
+    )
+    alternate_phone = PhoneNumberField(
+        region="IN",
+        max_length=13,
+        null=True,
+        blank=True,
+        verbose_name="Alternate Mobile Number (optional)",
+    )
+
+    class Meta:
+        verbose_name = "User address"
+        verbose_name_plural = "User addresses"
+
+    def __str__(self):
+        return self.name
+
+    @admin.display(description="Is Guest")
+    def is_guest(self):
+        """
+        Returns True if the address is associated with a guest user, otherwise False.
+        """
+        return self.guest_user is not None
+
+    is_guest.boolean = True
